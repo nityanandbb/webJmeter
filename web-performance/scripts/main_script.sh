85663f23c75ce03d55340c6094dae1d051aaa809
nityanandbb <nityanand.bharati@qed42.com>
2024-10-07 22:40:37 +0530

    test files.

--- web-performance/scripts/main_script.sh
+++ web-performance/scripts/main_script.sh
@@ -4,11 +4,12 @@
 source ./web-performance/scripts/jmx_executor.sh
 
 testList=("T5.jmx|" "new_assignment1.jmx|web-performance/jmeter/tests/performance/" "testfile2|/path/to/folder2/" "qedco.jmx|web-performance/jmeter/tests/performance/" "qed42.jmx|" "udv.jmx|" "Tem1.jmx|")
+testList1=("Tem1.jmx|")
 # Set the thread count
 THREAD_COUNT="14"
 export THREAD_COUNT
 echo "THREAD_COUNT input : $THREAD_COUNT"
 source ./web-performance/scripts/jmx_executor.sh
-get_test_files_for_execution "${testList[@]}"
+get_test_files_for_execution "${testList1[@]}"
 
 # act workflow_dispatch -e '{"threadnumber": 10, "testfilesList": "testfile1|/path/to/folder1/ testfile2|/path/to/folder2/ "Literature.jmx|web-performance/jmeter/tests/performance/" "Orders.jmx|", "baseurl": "http://example.com", "timeDuration": "1h"}'
